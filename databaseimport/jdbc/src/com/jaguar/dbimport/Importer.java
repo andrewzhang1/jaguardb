b8c6bf2e18532475149a05f41347cf840b2af714
hjshi <hjshi@yahoo.com>
2017-08-11 20:08:42 -0700

    rename to Importer.java

--- databaseimport/jdbc/src/com/jaguar/dbimport/Importer.java
+++ databaseimport/jdbc/src/com/jaguar/dbimport/Importer.java
@@ -1,4 +1,4 @@
-package com.datajugar.dbimport;
+package com.jaguar.dbimport;
 
 import java.io.FileReader;
 import java.sql.Connection;
@@ -9,48 +9,48 @@ import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.Properties;
 
-import javax.sql.DataSource;
-
-import com.jaguar.jdbc.JaguarDataSource;
-
-public class Test {
+public class Importer {
     public static void main(String[] args) throws Exception {
 
     	String appConf = System.getProperty("app.conf");
     	if (appConf == null) {
-    		System.err.println("Usage: java -cp jar1:jar2:... -Dapp.conf=<config_file> com.datajugar.dbimport.Test");
+    		System.err.println("Usage: java -cp jar1:jar2:... -Dapp.conf=<config_file> " + Importer.class.getName());
     		return;
     	}
     	
     	Properties appProp = new Properties();
     	appProp.load(new FileReader(appConf));
     	
-    	
-        // Jaguar database
-    	System.loadLibrary("JaguarClient");
-    	String jhost = appProp.getProperty("jaguar_host");
-    	String jport = appProp.getProperty("jaguar_port");
-    	String juser = appProp.getProperty("jaguar_user");
-    	String jpassword = appProp.getProperty("jaguar_password");
-    	String jdb = appProp.getProperty("jaguar_db");
-        DataSource ds = new JaguarDataSource(jhost, Integer.parseInt(jport), jdb);
-        Connection jc = ds.getConnection(juser, jpassword);
-        //PreparedStatement js = jc.prepareStatement("insert into pet values (?, ?, ?)");
-       
-        // mysql
+    	// load Jaguar driver
+    	try {
+    		Class.forName("com.jaguar.jdbc.JaguarDriver");
+    	}
+    	catch (Exception e) {
+    		e.printStackTrace();
+    	}
+ 
+        // source database
         String url = appProp.getProperty("source_jdbc_url") + appProp.getProperty("source_db");
-        System.out.println(url);
+        System.out.println("source " + url);
         String user = appProp.getProperty("source_user");
         String password = appProp.getProperty("source_password");
         String table = appProp.getProperty("source_table");
-        Connection connection = DriverManager.getConnection(url, user, password);
-        Statement st = connection.createStatement();
+        Connection sconn = DriverManager.getConnection(url, user, password);
+        Statement st = sconn.createStatement();
         ResultSet rs = st.executeQuery("select * from " + table);
         ResultSetMetaData meta = rs.getMetaData();
         System.out.println("column count=" + meta.getColumnCount());
         for (int i = 1; i <= meta.getColumnCount(); i++) {
             System.out.println(meta.getColumnName(i) + " has type " + meta.getColumnType(i));
         }
+        
+        // target database
+        url = appProp.getProperty("target_jdbc_url") + appProp.getProperty("target_db");
+        System.out.println("target" + url);
+        user = appProp.getProperty("target_user");
+        password = appProp.getProperty("target_password");
+        //String table = appProp.getProperty("source_table");
+        Connection tconn = DriverManager.getConnection(url, user, password);
         
         StringBuilder insertStatement = new StringBuilder("insert into " + table + " values(");
         for(int i = 1; i <= meta.getColumnCount(); i++) {
@@ -64,21 +64,22 @@ public class Test {
         
         System.out.println(insertStatement.toString());
         
-        PreparedStatement js = jc.prepareStatement(insertStatement.toString());
+        PreparedStatement ps = tconn.prepareStatement(insertStatement.toString());
         int rows = 0;
         while (rs.next()) {
-        	js.clearParameters();
+        	ps.clearParameters();
             for (int i = 1; i <= meta.getColumnCount(); i++) {
                 Object o = rs.getObject(i);
                 //System.out.println("class:" + o.getClass().getName() + ",value=" + o.toString());
-                js.setObject(i, o);
+                ps.setObject(i, o);
             }
-            int c = js.executeUpdate();
+            ps.executeUpdate();
             rows++;
             //System.out.println("update=" + c);
         }
         
-        jc.close();
+        sconn.close();
+        tconn.close();
         
         System.out.println("total " + rows + " imported.");
     }
