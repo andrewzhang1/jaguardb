afeadf4e97e0bc241a2a76ad71e8f84cc3dcd587
jonyue <jonathan.yue@datajaguar.com>
2017-09-06 21:10:32 -0400

    mysql trigger copy all columns frok OLD to NEW table

--- databasesync/mysql/MySQLToJaguar/trigger_replication.sql
+++ databasesync/mysql/MySQLToJaguar/trigger_replication.sql
@@ -2,50 +2,46 @@
 Suppose your table to be replicated is table123 with columns uid and addr.
 You can create a table123_trigger_table to capture the changes in table123.
 
+drop table if exists table123_trigger_table;
 create table table123_trigger_table (
-    ts datetime primary key,
+    ts datetime,
     uid int,
     addr varchar(64),
-    action char(1)
+    action char(1),
+	primary key( ts, uid )
 );
 
 
 Then you can create three triggers to record the changes of table123 into table123_trigger_table:
 
 DELIMITER $$
+DROP TRIGGER IF EXISTS after_table123_insert;
 CREATE TRIGGER after_table123_insert AFTER  INSERT ON table123 FOR EACH ROW
 BEGIN
-     INSERT INTO table123_trigger_table
-     SET action = 'I',
-        uid = NEW.uid,
-        addr = NEW.addr,
-        ts = NOW();
+     INSERT INTO table123_trigger_table select NOW(), table123.*, 'I' from table123 where uid = NEW.uid;
 END$$
 DELIMITER ;
 
 
 DELIMITER $$
+DROP TRIGGER IF EXISTS after_table123_update;
 CREATE TRIGGER after_table123_update AFTER  UPDATE ON table123 FOR EACH ROW
 BEGIN
-     INSERT INTO table123_trigger_table
-     SET action = 'U',
-        uid = NEW.uid,
-        addr = NEW.addr,
-        ts = NOW();
+     INSERT INTO table123_trigger_table select NOW(), table123.*, 'U' from table123 where uid = NEW.uid;
 END$$
 DELIMITER ;
 
 
 DELIMITER $$
+DROP TRIGGER IF EXISTS after_table123_delete;
 CREATE TRIGGER after_table123_delete AFTER  DELETE ON table123 FOR EACH ROW
 BEGIN
      INSERT INTO table123_trigger_table
-     SET action = 'D',
-        uid = OLD.uid,
-        addr = OLD.addr,
-        ts = NOW();
+	     set ts = NOW(), uid = OLD.uid, 
+		     action='D';
 END$$
 DELIMITER ;
+
 
 
 Finally, you just need to write a Java program to periodically pull the records
