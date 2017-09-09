d56da03849036d00a2373628e68083b0b5cbd827
hjshi <hjshi@yahoo.com>
2017-09-09 14:44:31 -0700

    replace tab with spaces.

--- databaseimport/jdbc/src/com/jaguar/dbimport/Importer.java
+++ databaseimport/jdbc/src/com/jaguar/dbimport/Importer.java
@@ -12,22 +12,22 @@ import java.util.Properties;
 public class Importer {
     public static void main(String[] args) throws Exception {
 
-    	String appConf = System.getProperty("app.conf");
-    	if (appConf == null) {
-    		System.err.println("Usage: java -cp jar1:jar2:... -Dapp.conf=<config_file> " + Importer.class.getName());
-    		return;
-    	}
-    	
-    	Properties appProp = new Properties();
-    	appProp.load(new FileReader(appConf));
-    	
-    	// load Jaguar driver
-    	try {
-    		Class.forName("com.jaguar.jdbc.JaguarDriver");
-    	}
-    	catch (Exception e) {
-    		e.printStackTrace();
-    	}
+        String appConf = System.getProperty("app.conf");
+        if (appConf == null) {
+            System.err.println("Usage: java -cp jar1:jar2:... -Dapp.conf=<config_file> " + Importer.class.getName());
+            return;
+        }
+        
+        Properties appProp = new Properties();
+        appProp.load(new FileReader(appConf));
+        
+        // load Jaguar driver
+        try {
+            Class.forName("com.jaguar.jdbc.JaguarDriver");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
  
         // source database
         String url = appProp.getProperty("source_jdbc_url") + appProp.getProperty("source_db");
@@ -54,12 +54,12 @@ public class Importer {
         
         StringBuilder insertStatement = new StringBuilder("insert into " + table + " values(");
         for(int i = 1; i <= meta.getColumnCount(); i++) {
-        	if (i == meta.getColumnCount()) {
-        		insertStatement.append("?)");
-        	}
-        	else {
-        		insertStatement.append("?,");
-        	}
+            if (i == meta.getColumnCount()) {
+                insertStatement.append("?)");
+            }
+            else {
+                insertStatement.append("?,");
+            }
         }
         
         System.out.println(insertStatement.toString());
@@ -67,7 +67,7 @@ public class Importer {
         PreparedStatement ps = tconn.prepareStatement(insertStatement.toString());
         int rows = 0;
         while (rs.next()) {
-        	ps.clearParameters();
+            ps.clearParameters();
             for (int i = 1; i <= meta.getColumnCount(); i++) {
                 Object o = rs.getObject(i);
                 //System.out.println("class:" + o.getClass().getName() + ",value=" + o.toString());
