b0c9ae5ce2f6fe75d73557c2152e0037bf6e113a
hjshi <hjshi@yahoo.com>
2017-09-11 21:30:43 -0700

    added id auto_increment as primary key.

--- databasesync/mysql/MySQLToJaguar/trigger_replication.sql
+++ databasesync/mysql/MySQLToJaguar/trigger_replication.sql
@@ -14,12 +14,13 @@ You can create a table123_trigger_table to capture the changes in table123.
 MySQL table:
 drop table if exists table123_trigger_table;
 create table table123_trigger_table (
-    ts_ datetime,
+    id_ int auto_increment primary key,
+    ts_ timestamp,
+    action_ char(1),
+    status_ char(1),
     nid int,
     addr varchar(64),
-	phone varchar(16),
-    action_ char(1),
-	primary key( ts_, nid )
+	phone varchar(16)
 );
 
 
@@ -29,8 +30,8 @@ DELIMITER $$
 DROP TRIGGER IF EXISTS after_table123_insert;
 CREATE TRIGGER after_table123_insert AFTER  INSERT ON table123 FOR EACH ROW
 BEGIN
-     INSERT INTO table123_trigger_table 
-	 select NOW(), table123.*, 'I' from table123 where nid = NEW.nid;
+     INSERT INTO table123_trigger_table (ts_, action_, status_, nid, addr, phone) 
+	 values( NOW(), 'I', 'I', new.nid, new.addr, new.phone);
 END$$
 DELIMITER ;
 
@@ -39,18 +40,18 @@ DELIMITER $$
 DROP TRIGGER IF EXISTS after_table123_update;
 CREATE TRIGGER after_table123_update AFTER  UPDATE ON table123 FOR EACH ROW
 BEGIN
-     INSERT INTO table123_trigger_table 
-	 select NOW(), table123.*, 'U' from table123 where nid = NEW.nid;
+     INSERT INTO table123_trigger_table (ts_, action_, status_, nid, addr, phone) 
+	values ( NOW(), 'U', 'I', new.nid, new.addr, new.phone); 
 END$$
 DELIMITER ;
 
 
 DELIMITER $$
-DROP TRIGGER IF EXISTS after_table123_delete;
-CREATE TRIGGER after_table123_delete AFTER  DELETE ON table123 FOR EACH ROW
+DROP TRIGGER IF EXISTS before_table123_delete;
+CREATE TRIGGER before_table123_delete AFTER  DELETE ON table123 FOR EACH ROW
 BEGIN
-     INSERT INTO table123_trigger_table
-	     set ts_ = NOW(), nid = OLD.nid, action_='D';
+     INSERT INTO table123_trigger_table (ts_, action_, status_, nid, addr, phone)
+	values ( NOW(), 'D', 'I', old.nid, old.addr, old.phone);
 END$$
 DELIMITER ;
 
