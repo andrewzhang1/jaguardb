f5288bd7af68cccedab266d925ecd5521bee3d08
jonyue <jonathan.yue@datajaguar.com>
2017-09-14 16:50:47 -0400

    added import_from_mysql.sh import_from_postgresql.sh

---
+++ databaseimport/jdbc/import_from_postgresql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+JARS=lib/postgresql-42.1.4.jar:lib/ojdbc6.jar:lib/mysql-connector-java-5.1.43-bin.jar:lib/jaguar-jdbc-2.0.jar:lib/dbimport.jar 
+
+nohup java -cp $JARS -Dapp.conf=app.conf.postgresql com.jaguar.dbimport.Importer >> import_jdbc_db.log &
+
