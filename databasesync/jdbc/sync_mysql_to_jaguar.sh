503683e13fe5cf66b996275447c876f4f9d4c0f2
jonyue <jonathan.yue@datajaguar.com>
2017-09-14 18:40:14 -0700

    rm sync_oracle_to_jaguar.sh  in wrong dir

--- databasesync/jdbc/sync_mysql_to_jaguar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nohup java -cp lib/mysql-connector-java-5.1.43-bin.jar:lib/jaguar-jdbc-2.0.jar:lib/dbsync.jar \
-  -Dapp.conf=app.conf.mysql \
-  com.jaguar.dbsync.Sync >> mysql_to_jaguar.log &
-
-
--- databasesync/jdbc/sync_oracle_to_jaguar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-nohup java -cp lib/ojdbc6.jar:lib/jaguar-jdbc-2.0.jar:lib/dbsync.jar \
-    -Dapp.conf=app.conf.oracle \
-	com.jaguar.dbsync.Sync >> oracle_to_jaguar.log &
-
