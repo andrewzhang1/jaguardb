07ff72920d621b6b21ba25bf4a60757ca4906969
jonyue <jonathan.yue@datajaguar.com>
2017-09-14 23:26:55 -0700

    added stop_sync_oracle_to_jaguar.sh

--- databasesync/oracle/OracleToJaguar/example1/start_sync_oracle_to_jaguar.sh
+++ databasesync/oracle/OracleToJaguar/example1/start_sync_oracle_to_jaguar.sh
@@ -22,6 +22,7 @@ if [[ -f "java.lock" ]]; then
 fi
 
 touch java.lock
+sed -i "s/.*stop=.*/#stop=true/g" app.conf.oracle
 
 java -cp $LIB/ojdbc6.jar:$LIB/jaguar-jdbc-2.0.jar:$LIB/dbsync.jar \
     -Dapp.conf=app.conf.oracle com.jaguar.dbsync.Sync >> sync_oracle_to_jaguar.log &
